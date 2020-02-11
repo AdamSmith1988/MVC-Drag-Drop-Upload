b2c808a7959f84b4054caf36b9c8c23df1e236de
Adam Smith <Amith06@hotmail.co.uk>
2020-02-11 22:52:19 +0000

    Initial Commit

---
+++ MVCDragDropUpload/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDragDropUpload.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UploadFiles(IEnumerable<HttpPostedFileBase> files)
+        {
+            foreach (var file in files)
+            {
+                string filePath = Guid.NewGuid() + Path.GetExtension(file.FileName);
+                file.SaveAs(Path.Combine(Server.MapPath("~/UploadedFiles"), filePath));
+            }
+
+            return Json("file uploaded successfully");
+        }
+    }
+}
